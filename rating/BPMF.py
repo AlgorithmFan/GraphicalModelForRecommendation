6c2d36f7786b3c765f9208b03f04ed11e17e2efe
zhang.haidong <haidong_zhang13@163.com>
2016-03-17 09:28:03 +0800

    Bayesian Probabilistic Matrix Factorization

--- rating/BPMF.py
+++ rating/BPMF.py
@@ -1,6 +1,9 @@
 #!usr/bin/env python
 #coding:utf-8
 
+from scipy.sparse import dok_matrix
+import numpy as np
+
 from Recommender import Recommender
 
 class BayesianProbabilisticMatrixFactorization(Recommender):
@@ -11,8 +14,21 @@ class BayesianProbabilisticMatrixFactorization(Recommender):
         super.__init__(trainMatrix, testMatrix, configHandler)
 
     def initModel(self):
-        pass
+        self.numUsers, self.numItems = self.trainMatrix.shape()
+        self.prediction = dok_matrix((self.numUsers, self.numItems))
+        self.MAX_Iterations = int(self.configHandle.getParameter('BPMF', 'MAX_Iterations'))
+        self.numFactors = int(self.configHandle.getParameter('BPMF', 'numFactors'))
+        self.learnRate = float(self.configHandle.getParameter('BPMF', 'learning_rate'))
+        self.regU = float(self.configHandle.getParameter('BPMF', 'regU'))
+        self.regI = float(self.configHandle.getParameter('BPMF', 'regI'))
+
+        self.P = np.random.normal(0, 1, size=(self.numUsers, self.numFactors))
+        self.Q = np.random.normal(0, 1, size=(self.numItems, self.numFactors))
 
 
     def buildModel(self):
+        pass
+
+
+    def predict(self, u, i):
         pass
