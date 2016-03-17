2835eb5845dc6140172deca521d874936f4f85b6
zhang.haidong <haidong_zhang13@163.com>
2016-03-17 09:39:40 +0800

    Bayesian Probabilistic Matrix Factorization (not completed)

--- rating/BPMF.py
+++ rating/BPMF.py
@@ -28,6 +28,16 @@ class BayesianProbabilisticMatrixFactorization(Recommender):
 
     def buildModel(self):
 
+        # Hyper-Parameters of Users
+        mu0_U = np.zeros(self.numFactors)
+        nu0_U = np.zeros(self.numFactors)
+        wh0_U = np.eye(self.numFactors)
+
+        # Hyper-Parameters of Items
+        mu0_V = np.zeros(self.numFactors)
+        nu0_V = np.zeros(self.numFactors)
+        wh0_V = np.eye(self.numFactors)
+
 
 
         for u, i in self.testMatrix.keys():
