2c2f124fb4cfdf7c3bbe945c55a75c96c93beb0c
zhang.haidong <haidong_zhang13@163.com>
2016-04-02 15:37:30 +0800

    Building Model

--- ranking/PLSA.py
+++ ranking/PLSA.py
@@ -8,4 +8,18 @@ class PLSA(Recommender):
         super.__init__(trainMatrix, testMatrix, configHandler)
 
     def initModel(self):
-        pass
+        self.numUsers, self.numItems = self.trainMatrix.shape()
+        self.prediction = dok_matrix((self.numUsers, self.numItems))
+        self.MAX_Iterations = int(self.configHandler.getParameter('BPMF', 'MAX_Iterations'))
+        self.numFactors = int(self.configHandler.getParameter('BPMF', 'numFactors'))
+
+        self.beta0 = float(self.configHandler.getParameter('BPMF', 'beta0'))
+        self.nu0 = float(self.configHandler.getParameter('BPMF', 'nu0'))
+        self.wh0 = np.eye(self.numFactors)
+
+        self.learnRate = float(self.configHandler.getParameter('BPMF', 'learning_rate'))
+        self.regU = float(self.configHandler.getParameter('BPMF', 'regU'))
+        self.regI = float(self.configHandler.getParameter('BPMF', 'regI'))
+
+        self.P = np.random.normal(0, 1, size=(self.numUsers, self.numFactors))
+        self.Q = np.random.normal(0, 1, size=(self.numItems, self.numFactors))
--- util/__init__.py
+++ util/__init__.py
@@ -2,3 +2,4 @@
 #coding:utf-8
 
 from readConfig import *
+from normalization import normalize
