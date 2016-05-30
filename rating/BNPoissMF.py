473c28865dcf8bf990607bb46e3c9b57246bdbfb
HaidongZhang <haidong_zhang13@163.com>
2016-05-30 17:31:19 +0800

    Framework of Bayessian Nonparametric Poisson Matrix Factorization

--- rating/BNPoissMF.py
+++ rating/BNPoissMF.py
@@ -21,15 +21,64 @@ class BNPoissMF(Recommender):
 
 
     def initModel(self):
-        pass
+        ''' Read the model parameters, and get some common values.
+        '''
+        self.numUsers, self.numItems = self.trainMatrix.shape()
+        self.prediction = dok_matrix((self.numUsers, self.numItems))
+        self.MAX_Iterations = int(self.configHandler.getParameter('BPoissMF', 'MAX_Iterations'))
+        self.numFactors = int(self.configHandler.getParameter('BPoissMF', 'numFactors'))
+        self.threshold = float(self.configHandler.getParameter('BPoissMF', 'threshold'))
+
+        # Get the Parameters
+        self.user_alpha = float(self.configHandler.getParameter('BPoissMF', 'user_alpha'))
+        self.user_c = float(self.configHandler.getParameter('BPoissMF', 'user_c'))
+
+        self.item_a = float(self.configHandler.getParameter('BPoissMF', 'item_a'))
+        self.item_b = float(self.configHandler.getParameter('BPoissMF', 'item_b'))
+
+        # The model parameters for users
+        self.gamma0 = np.zeros(self.numUsers)
+        self.gamma1 = np.zeros(self.numUsers)
+        self.s = np.zeros(self.numUsers)
+        self.nu = np.zeros((self.numUsers, self.numFactors))
+        self.theta = np.zeros((self.numUsers, self.numFactors))
+
+        # The model parameters for stick proportions
+        self.tau = np.zeros((self.numUsers, self.numFactors))
+
+        # The model parameters for item weights
+        self.lambda0 = np.zeros((self.numItems, self.numFactors))
+        self.lambda1 = np.zeros((self.numItems, self.numFactors))
+        self.beta = np.zeros((self.numItems, self.numFactors))
+
+        self.z = np.zeros((self.numUsers, self.numItems))
+
 
     def buildModels(self):
         pass
 
-    def updateUserPrior(self):
+    def initUserScalingParameters(self):
         pass
 
-    def updateItemPrior(self):
+
+    def initStickProportions(self):
+        ''' The update equations for the stick proportions tau_uk can be obtained by taking the derivative of the objective function with respect to tau_uk
+
+        '''
+
+    def initItemWeights(self):
+        pass
+
+    def updateUserScalingParameters(self):
+        pass
+
+    def updateStickProportions(self):
+        pass
+
+    def updateItemWeights(self):
+        pass
+
+    def calculateConjugacy(self):
         pass
 
     def GammaPoisson(self):
---
+++ util/AIC.py
@@ -0,0 +1,24 @@
+#!usr/bin/env python
+#coding:utf-8
+
+
+def calculateAIC(numParameters, likelihood):
+    return 2*numParameters - 2*likelihood
+
+
+
+if __name__ == '__main__':
+    def hmm(numStates, numItems):
+        return numStates * numStates + numStates * numItems + numStates + 2 * numStates
+
+    def ihmm(numStates, numItems):
+        return numStates * numStates + numStates * numItems + numStates + 2 * numStates + numStates
+
+
+    numStates = 30
+    numItems = 1621
+    numParameters = hmm(numStates, numItems)
+    print 'HMM: {}'.format(calculateAIC(numParameters, -2434502))
+
+    numParameters = ihmm(numStates, numItems)
+    print 'IHMM: {}'.format(calculateAIC(numParameters, -2337670))
